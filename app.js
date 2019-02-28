aa5c70e7d9c3734d4c8e2890c6629a3835dfb9ef
lyndemberg <lyndembergbatista@outlook.com>
2019-02-28 14:38:24 -0300

    using client credentials

--- app.js
+++ app.js
@@ -8,15 +8,30 @@ const logger = require('morgan');
 const SongAPI = require('./routes/songs-route');
 const SpotifyWebApi = require('spotify-web-api-node');
 var requestNumber = 0;
-const credentials = {
+
+var client = new SpotifyWebApi({
   clientId: process.env.ID,
-  clientSecret: process.env.SECRET,
-  redirectUri: process.env.REDIRECT_URI
-};
-var client = new SpotifyWebApi(credentials);
-//aux
-var tokenExpirationEpoch;
-var numberOfTimesUpdated = 0;
+  clientSecret: process.env.SECRET
+});
+client.clientCredentialsGrant().then(
+  function(data) {
+    console.log('The access token expires in ' + data.body['expires_in']);
+    console.log('The access token is ' + data.body['access_token']);
+
+    // Save the access token so that it's used in future calls
+    spotifyApi.setAccessToken(data.body['access_token']);
+  },
+  function(err) {
+    console.log(
+      'Something went wrong when retrieving an access token',
+      err.message
+    );
+  }
+);
+
+// //aux
+// var tokenExpirationEpoch;
+// var numberOfTimesUpdated = 0;
 const app = express();
 
 app.use(logger('dev'));
@@ -27,10 +42,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/song',SongAPI);
 app.get('/api/song/search',(req,res)=>{
-  if(requestNumber==0){
-    getCodeAuthorizathion();
-    refresh();
-  }
+  // if(requestNumber==0){
+  //   getCodeAuthorizathion();
+  //   refresh();
+  // }
   const query = req.query.q;
   client.searchTracks(query)
     .then((value)=>{
@@ -38,66 +53,81 @@ app.get('/api/song/search',(req,res)=>{
     },
     (err)=>{
       res.status(400).send({});
-    })
+    });
 });
-app.get('/callback',(req,res)=>{
-    const code = req.query.code;
-    console.log('RETORNOU-> '+code);
-    res.status(200).send({});
-    client.authorizationCodeGrant(code).then(
-      (data)=> {
-          console.log('The token expires in ' + data.body['expires_in']);
-          console.log('The access token is ' + data.body['access_token']);
-          console.log('The refresh token is ' + data.body['refresh_token']);
+// app.get('/callback',(req,res)=>{
+//     const code = req.query.code;
+//     console.log('RETORNOU-> '+code);
+//     res.status(200).send({});
+//     client.authorizationCodeGrant(code).then(
+//       (data)=> {
+//           console.log('The token expires in ' + data.body['expires_in']);
+//           console.log('The access token is ' + data.body['access_token']);
+//           console.log('The refresh token is ' + data.body['refresh_token']);
       
-          client.setAccessToken(data.body['access_token']);
-          client.setRefreshToken(data.body['refresh_token']);
+//           client.setAccessToken(data.body['access_token']);
+//           client.setRefreshToken(data.body['refresh_token']);
   
-          tokenExpirationEpoch = new Date().getTime() / 1000 + data.body['expires_in'];
-          console.log(
-              'Retrieved token. It expires in ' +
-              Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) +
-              ' seconds!'
-          );
-      },
-      (err)=> {
-          console.log('Something went wrong!', err);
-      });
-});
+//           tokenExpirationEpoch = new Date().getTime() / 1000 + data.body['expires_in'];
+//           console.log(
+//               'Retrieved token. It expires in ' +
+//               Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) +
+//               ' seconds!'
+//           );
+//       },
+//       (err)=> {
+//           console.log('Something went wrong!', err);
+//       });
+// });
 
-function getCodeAuthorizathion(){
-  console.log('autorizando o client');
-}
+// function getCodeAuthorizathion(){
+//   https.get('https://syncband.herokuapp.com/api/song/search?q=nothing', (resp) => {
+//   let data = '';
 
-function refresh(){
-  setInterval(function() {
-    console.log(
-      'Time left: ' +
-        Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) +
-        ' seconds left!'
-    );
+//   // A chunk of data has been recieved.
+//   resp.on('data', (chunk) => {
+//     data += chunk;
+//   });
+
+//   // The whole response has been received. Print out the result.
+//   resp.on('end', () => {
+//     console.log(JSON.parse(data).explanation);
+//   });
+
+//   }).on("error", (err) => {
+//     console.log("Error: " + err.message);
+//   });
+// }
+
+// function refresh(){
+//   setInterval(function() {
+//     console.log(
+//       'Time left: ' +
+//         Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) +
+//         ' seconds left!'
+//     );
   
-    // OK, we need to refresh the token. Stop printing and refresh.
-    if (++numberOfTimesUpdated > 5) {
-      clearInterval(this);
+//     // OK, we need to refresh the token. Stop printing and refresh.
+//     if (++numberOfTimesUpdated > 5) {
+//       clearInterval(this);
   
-      // Refresh token and print the new time to expiration.
-      client.refreshAccessToken().then(
-        function(data) {
-          tokenExpirationEpoch =
-            new Date().getTime() / 1000 + data.body['expires_in'];
-          console.log(
-            'Refreshed token. It now expires in ' +
-              Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) +
-              ' seconds!'
-          );
-        },
-        function(err) {
-          console.log('Could not refresh the token!', err.message);
-        }
-      );
-    }
-  }, 1000);
-}
+//       // Refresh token and print the new time to expiration.
+//       client.refreshAccessToken().then(
+//         function(data) {
+//           tokenExpirationEpoch =
+//             new Date().getTime() / 1000 + data.body['expires_in'];
+//           console.log(
+//             'Refreshed token. It now expires in ' +
+//               Math.floor(tokenExpirationEpoch - new Date().getTime() / 1000) +
+//               ' seconds!'
+//           );
+//         },
+//         function(err) {
+//           console.log('Could not refresh the token!', err.message);
+//         }
+//       );
+//     }
+//   }, 1000);
+// }
 
 module.exports = app;
